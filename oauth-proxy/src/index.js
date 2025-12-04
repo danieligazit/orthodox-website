a47093e96d3c98e0997f3bb7ad0ba2494b22f143
danielgazit <daniel.gazit@island.io>
2025-12-04 09:50:16 +0100

    fix auth 4

--- oauth-proxy/src/index.js
+++ oauth-proxy/src/index.js
@@ -12,7 +12,7 @@ export default {
     const corsHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     };
 
     // Handle preflight requests
