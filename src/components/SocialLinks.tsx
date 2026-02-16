d4d00d72000b39985f00e0a266e3ef7fbb91ba30
danielgazit <daniel.gazit@island.io>
2026-02-16 23:25:24 +0100

    fix icons

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -7,14 +7,15 @@ interface SocialLink {
 }
 
 // SoundCloud icon SVG component
+// Official SoundCloud logo from SVG Repo
 const SoundCloudIcon = ({ className }: { className?: string }) => (
   <svg
     className={className}
-    viewBox="0 0 24 24"
+    viewBox="0 0 20 20"
     fill="currentColor"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M12.017 0C8.396 0 5.29 2.154 3.594 5.31c-.094.188-.156.375-.188.563-.656-.375-1.406-.563-2.25-.563C.469 5.31 0 5.779 0 6.373v11.25c0 .594.469 1.063 1.156 1.063h10.875c.375 0 .75-.094 1.031-.281 1.125-.75 1.875-2.063 1.875-3.563 0-1.125-.469-2.156-1.219-2.906.281-.656.469-1.406.469-2.156 0-2.625-2.156-4.781-4.781-4.781zm7.594 7.5c-.188 0-.375.094-.563.188-.656-.938-1.688-1.5-2.813-1.5-.375 0-.75.094-1.031.188C14.017 5.625 12.892 5.06 11.642 5.06c-1.969 0-3.563 1.594-3.563 3.563 0 .188 0 .375.094.563-.469.281-.75.844-.75 1.406v4.5c0 .844.656 1.5 1.5 1.5h9.75c1.969 0 3.563-1.594 3.563-3.563 0-1.969-1.594-3.563-3.563-3.563z" />
+    <path d="M.672 13.055L1 11.654l-.328-1.447c-.009-.043-.092-.076-.191-.076-.102 0-.184.033-.191.076L0 11.654l.289 1.4c.008.045.09.076.191.076.1.001.183-.03.192-.075zm2.051.777L3 11.668 2.723 8.32c-.009-.084-.114-.152-.239-.152-.127 0-.233.068-.238.152L2 11.668l.246 2.164c.006.086.111.152.238.152.125 0 .23-.066.239-.152zm2.045-.035L5 11.67l-.232-4.457c-.006-.106-.129-.188-.282-.188-.152 0-.275.082-.281.188L4 11.67l.205 2.129c.006.103.129.186.281.186.153-.001.276-.083.282-.188zm2.042-.031L7 11.67l-.19-4.49c-.005-.123-.146-.221-.32-.221-.176 0-.316.098-.321.221L6 11.67l.17 2.096c.004.123.145.221.32.221.174-.001.315-.096.32-.221zm2.04-.028L9 11.672l-.15-5.149c-.004-.142-.164-.255-.358-.255s-.354.115-.357.256L8 11.67l.135 2.068c.003.141.163.256.357.256.194 0 .354-.113.358-.256zm1.427.258l7.145.004C18.846 14 20 12.883 20 11.506s-1.154-2.492-2.578-2.492c-.353 0-.689.07-.996.193-.205-2.246-2.153-4.008-4.529-4.008a4.77 4.77 0 0 0-1.648.297c-.196.074-.247.148-.249.295v7.91a.306.306 0 0 0 .277.295z" />
   </svg>
 );
 
@@ -34,7 +35,7 @@ const socialLinks: SocialLink[] = [
   {
     name: 'SoundCloud',
     url: 'https://soundcloud.com/orthodoxrecords',
-    icon: <SoundCloudIcon className="w-4 h-4" />,
+    icon: <SoundCloudIcon className="w-5 h-5" />,
   },
   {
     name: 'Instagram',
@@ -63,14 +64,14 @@ export function SocialLinks() {
             href={link.url}
             target="_blank"
             rel="noopener noreferrer"
-            className="group flex items-center gap-2.5 text-[#444] hover:text-[#050505] transition-colors duration-300"
+            className="group flex items-center gap-2.5 text-[#444] hover:text-[#e8e6df] transition-colors duration-300"
           >
-            <span className="text-[#666] group-hover:text-[#050505] transition-colors duration-300">
+            <span className="text-[#666] group-hover:text-[#e8e6df] transition-colors duration-300">
               {link.icon}
             </span>
             <span className="relative font-im-fell text-sm uppercase tracking-widest">
               {link.name}
-              <span className="absolute left-0 bottom-0 w-full h-px bg-[#050505] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+              <span className="absolute left-0 top-1/2 w-full h-px bg-[#e8e6df] transform -translate-y-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></span>
             </span>
           </a>
         ))}
